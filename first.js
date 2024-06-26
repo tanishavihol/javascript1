1f5719fbd2f7462aecf69cd7efe91964e8c68af5
tanishavihol <157605807+tanishavihol@users.noreply.github.com>
2024-06-26 21:52:00 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,36 @@
+fullname="tony stark";
+console.log(fullname);
+let age=24;
+console.log(age)
+price=99.99;
+console.log(price);
+x=null;
+y=undefined;
+console.log(x);
+isFollow=false;
+console.log(isFollow);
+let num="24";
+num="25";
+num="35";
+console.log(num);
+const PI="3.14";
+console.log(PI);
+let a=BigInt("123");
+console.log(BigInt);
+const student={
+fullName:"rahul",
+age:20,
+cgpa:8.2,
+isPass:true,
+};
+student["age"]=student["age"]+1;
+console.log(student["age"]);
+student["name"]="rahul sharma";
+console.log(student["name"]);
+const profile={
+   username:"@shradhakhapra",
+   isFollow:false,
+   followers:123,
+   following:123
+}
+console.log(typeof profile["username"]);
